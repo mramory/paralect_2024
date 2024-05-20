e40a2007b08c5d39c9b8885f3119d7c947c57238
mramory <119244930+mramory@users.noreply.github.com>
2024-05-20 20:35:37 +0300

    test movies loading

--- src/app/(movies)/page.tsx
+++ src/app/(movies)/page.tsx
@@ -5,7 +5,7 @@ import { Pagination } from "@/components/Pagination"
 import { getGenres, getMovies } from "@/requests/movies";
 
 export default async function HomePage({searchParams}: {searchParams: { [key: string]: string | string[] | undefined }}) {
-  const movies = await getMovies(searchParams)
+  const movies = getMovies(searchParams)
   const genres = await getGenres()
 
   return (
@@ -13,8 +13,8 @@ export default async function HomePage({searchParams}: {searchParams: { [key: st
       <h1>Movies</h1>
       <div>
         <Filters genres={genres.body.genres} />
-        <MoviesList page="movies" genres={genres.body.genres} initialData={movies} />
-        {movies.body.total_results ? <Pagination defaultPage={movies.body.page} total={movies.body.total_pages} align="end" /> : null}
+        <MoviesList page="movies" genres={genres.body.genres} />
+        {(await movies).body.total_results ? <Pagination defaultPage={(await movies).body.page} total={(await movies).body.total_pages} align="end" /> : null}
       </div>
     </main>
   );
